5b5e2f881223ef89be559d2a8da81e51b9543ed5
Mert Kaan İrtegün <irtegun16@itu.edu.tr>
2023-01-06 21:56:15 +0300

    meals app is started

---
+++ screen/CategoriesScreen.js
@@ -0,0 +1,9 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+
+
+const CategoriesScreen = () => {
+    return <FlatList data={CATEGORIES} />
+}
+
+export default CategoriesScreen;
