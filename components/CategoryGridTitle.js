7d9af8ebbcde4fa1205c411afd1101ae29021452
Mert Kaan İrtegün <irtegun16@itu.edu.tr>
2023-01-06 22:03:07 +0300

    Added CategoryGridTile

---
+++ components/CategoryGridTitle.js
@@ -0,0 +1,18 @@
+import { Pressable, Text, View } from "react-native"
+
+
+
+
+const CategoryGridTitle = ({title, color}) => {
+    return <View>
+        <Pressable>
+            <View>
+                <Text>
+                    {title}
+                </Text>
+            </View>
+        </Pressable>
+    </View>
+}
+
+export default CategoryGridTitle;
--- screen/CategoriesScreen.js
+++ screen/CategoriesScreen.js
@@ -1,9 +1,13 @@
 import { FlatList } from "react-native";
+import CategoryGridTitle from "../components/CategoryGridTitle";
 import { CATEGORIES } from "../data/dummy-data";
 
+const renderCategoryItem = (itemData) => {
+    return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color}></CategoryGridTitle>
+}
 
 const CategoriesScreen = () => {
-    return <FlatList data={CATEGORIES} />
-}
+  return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />;
+};
 
 export default CategoriesScreen;
